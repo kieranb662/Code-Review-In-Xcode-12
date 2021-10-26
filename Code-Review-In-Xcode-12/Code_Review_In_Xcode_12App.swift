85e2fed88c29dddee586cd46dd1b5bb44a6ea1cb
Kieran Brown <kb6627500@gmail.com>
2021-10-26 12:47:45 -0400

    Initial Commit

---
+++ Code-Review-In-Xcode-12/Code_Review_In_Xcode_12App.swift
@@ -0,0 +1,17 @@
+// Swift toolchain version 5.0
+// Running macOS version 12.0
+// Created on 10/26/21.
+//
+// Author: Kieran Brown
+//
+
+import SwiftUI
+
+@main
+struct Code_Review_In_Xcode_12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Code-Review-In-Xcode-12/ContentView.swift
@@ -0,0 +1,21 @@
+// Swift toolchain version 5.0
+// Running macOS version 12.0
+// Created on 10/26/21.
+//
+// Author: Kieran Brown
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
