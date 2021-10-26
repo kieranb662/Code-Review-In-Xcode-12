85d0c4a1ba3dac5d587cdd7d2cf01de6cd8c7a78
Kieran Brown <kb6627500@gmail.com>
2021-10-26 13:25:18 -0400

    Modified text to match new designs on content view.

--- Code-Review-In-Xcode-12/ContentView.swift
+++ Code-Review-In-Xcode-12/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("This is kieran's content view now!")
             .padding()
     }
 }
